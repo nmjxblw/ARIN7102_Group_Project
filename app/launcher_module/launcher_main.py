db871a8d7d9f977be3fc268840607d01824a9745
z c <915253492@qq.com>
2026-03-28 04:45:05 +0800

    Epoch 10 Summary: Train Loss: 0.0591 | Val Loss: 0.1606 Task Losses - Disease: 0.0333, Symptom: 0.0112, FirstAid: 0.1316 Disease - F1: 0.7778 | Precision: 0.8184 | Recall: 0.7411 Symptom F1: 0.9079 | Emergency Acc: 0.9710

--- app/launcher_module/launcher_main.py
+++ app/launcher_module/launcher_main.py
@@ -114,6 +114,6 @@ def end_background_threads():
 def app_run() -> None:
     """主程序入口"""
 
-    from evaluation import generate_eval_dataset_llm_main
+    from deployment_module import train_bert
 
-    generate_eval_dataset_llm_main()
+    train_bert()
