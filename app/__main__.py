c93c87a734409136694deb08cc49de1e65dd62f5
yaoja123 <yaoja123@gmail.com>
2026-05-01 11:04:22 +0800

    This is pruned but not refactor, keeping the necessary runnable code

--- app/__main__.py
+++
@@ -1,9 +0,0 @@
-if __name__ == "__main__":
-    print(f"{__package__}.{__name__} 被作为主程序运行，启动 launcher 模块...")
-
-    profile_filename = f"app_runtime_result.stats"
-
-    # 本地模块导入
-    from launcher_module import app_run
-
-    app_run()
