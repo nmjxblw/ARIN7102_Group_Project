f5c8edad107a76c76f5753ac2c341a031a230aec
PeaHaWf <1161428420@qq.com>
2026-05-04 00:33:54 +0800

    rename

--- app/deployment_module/__init__.py
+++ app/deployment_module/__init__.py
@@ -6,6 +6,6 @@
 也可以在这里写点 Transformer/Bert 之类的模型
 """
 
-from ._bert_manager_improved import *
+from .bert_manager import *
 
 __all__ = ["BERTManager"]
--- app/ui_module/streamlit_app.py
+++ app/ui_module/streamlit_app.py
@@ -11,7 +11,7 @@ from openai import OpenAI
 # Ensure other modules in the project can be imported
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from deployment_module._bert_manager_improved import BERTManager
+from deployment_module import BERTManager
 from evaluation_module import DrugRecommendationService
 from static_module import DEEPSEEK_API_KEY, DEEPSEEK_MODEL
 
